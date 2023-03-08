a3e07fb0746ee9667b922a44716921f244e51c75
Woopsoops <parviainen.joni@gmail.com>
2023-03-08 12:46:00 +0200

    simple calculator

---
+++ App.js
@@ -0,0 +1,65 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput} from 'react-native';
+
+export default function App() {
+  const [number1, setNumber1] = useState('');
+  const [number2, setNumber2] = useState('');
+  const [total, setTotal] = useState('');
+
+
+  function calculateAddition() {
+    setTotal(parseInt(number1) + parseInt(number2));
+  }
+
+  function calculateSubtraction() {
+    setTotal(parseInt(number1) - parseInt(number2));
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.container}>
+      <Text style={{fontSize: 20}}>Result: {total} </Text>
+
+        <TextInput keyboardType="numeric" style={styles.input} value={number1} onChangeText={number1 => setNumber1(number1)}/>
+        <TextInput keyboardType="numeric" style={styles.input} value={number2} onChangeText={number2 => setNumber2(number2)} />
+      </View>
+
+      <View style={styles.button}>
+        <Button
+          title="+"
+          onPress={calculateAddition}
+        />
+        <Button
+          title="-"
+          onPress={calculateSubtraction}
+        />
+        </View>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#8c92ac',
+    alignItems: 'center',
+    justifyContent: 'flex-end'
+  },
+  input : {
+    fontSize: 22,
+    width:200  , 
+    borderColor: '#bf5700', 
+    borderWidth: 1,
+    padding: 20,
+  },
+  button: { 
+    flex: 2,
+    width: 180,
+    flexDirection: "row", 
+    justifyContent: 'space-around',
+    alignItems: 'flex-start',
+    padding: 30,
+  }
+
+})
+;
